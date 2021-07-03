cea5290a78cfde842d2dc19875b850012d270767
M.Imran-Invozone <mimranisrar6@gmail.com>
2021-07-03 14:28:37 +0500

    ssl implement

--- config/websockets.php
+++ config/websockets.php
@@ -116,13 +116,13 @@ return [
          * certificate chain of issuers. The private key also may be contained
          * in a separate file specified by local_pk.
          */
-        'local_cert' => null,
+        'local_cert' => '/etc/letsencrypt/live/mimran.xyz/fullchain.pem',
 
         /*
          * Path to local private key file on filesystem in case of separate files for
          * certificate (local_cert) and private key.
          */
-        'local_pk' => null,
+        'local_pk' => '/etc/letsencrypt/live/mimran.xyz/privkey.pem',
 
         /*
          * Passphrase for your local_cert file.
